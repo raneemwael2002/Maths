7dc4da9a97582ba9f4639fe1d8aae8872d0f6aac
raneemwael2002 <66146343+raneemwael2002@users.noreply.github.com>
2024-11-11 21:00:06 +0200

    Trail.cpp

--- Trail.cpp
+++ Trail.cpp
@@ -19,20 +19,20 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Multiple Balls");
 
-    // Number of balls
+   
     const int numBalls = 5;
 
-    // Initialize each ball shape and set origin to the center
+
     std::vector<sf::CircleShape> balls(numBalls, sf::CircleShape(20));
     for (auto& ball : balls)
         ball.setOrigin(20, 20);
 
-    // Set initial positions for each ball
+   
     std::vector<sf::Vector2f> positions(numBalls, sf::Vector2f(20, 20));
     std::vector<sf::Vector2f> startPositions(numBalls, sf::Vector2f(20, 20));
     std::vector<sf::Vector2f> targetPositions(numBalls, sf::Vector2f(20, 20));
 
-    // Ball colors (can vary for each ball)
+   
     std::vector<sf::Color> colors = { sf::Color::Red, sf::Color::Green, sf::Color::Blue, sf::Color::Yellow, sf::Color::Magenta };
     for (int i = 0; i < numBalls; ++i)
         balls[i].setFillColor(colors[i]);
@@ -40,7 +40,7 @@ int main()
     sf::Clock clock;
     float time;
 
-    // Increase duration for each ball to make the following effect slower
+    
     std::vector<float> durations = { 0.5, 0.7, 0.9, 1.1, 1.3 };
 
     while (window.isOpen())
@@ -53,7 +53,7 @@ int main()
 
             if (event.type == sf::Event::MouseMoved)
             {
-                // Update target position of the main ball
+                
                 targetPositions[0] = (sf::Vector2f)sf::Mouse::getPosition(window);
                 startPositions[0] = positions[0];
                 clock.restart();
@@ -62,17 +62,17 @@ int main()
 
         time = clock.getElapsedTime().asSeconds();
 
-        // Update each ball's position
+        
         for (int i = 0; i < numBalls; ++i)
         {
             float alpha = time / durations[i]; // Use a different duration for each ball
 
-            // For the first ball, follow the mouse target
+           
             if (i == 0)
             {
                 positions[i] = lerp(startPositions[i], targetPositions[i], alpha);
             }
-            // For other balls, follow the previous ball with a slower approach
+            
             else
             {
                 targetPositions[i] = positions[i - 1];
@@ -80,12 +80,12 @@ int main()
                 positions[i] = lerp(startPositions[i], targetPositions[i], alpha);
             }
 
-            // Update the position of the shape
+           
             balls[i].setPosition(positions[i]);
         }
 
         window.clear();
-        // Draw each ball
+       
         for (const auto& ball : balls)
             window.draw(ball);
 
